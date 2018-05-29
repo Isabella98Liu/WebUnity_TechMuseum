9cd3108ec5bc659dd3e9ee734fdb1d015c52c4ac
Isabella98Liu <isabellaliu98@berkeley.edu>
2018-05-29 22:36:32 +0800

    【ADD】3D objects list & Scoll bar

---
+++ Assets/Scripts/GlobalMono.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalMono : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public class Global
+{
+    public static bool ScrollBar = false;   // whether current user is scrolling the object list window
+}
+
